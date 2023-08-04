14c99b15794151947559b4c81877af9ada1e7455
Jake Goldman <jgoldman@caltech.edu>
2023-08-03 18:03:45 -0700

    Fully generalize posterior pair plots

--- python/posteriors.py
+++ python/posteriors.py
@@ -3,16 +3,18 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import defaultdict
-from typing import Tuple
+from typing import Tuple, List
 import queue
+
+PROB_LABEL = 'p'
 
 # MapData class is defined to store information for heatmaps
 class MapData:
-    def __init__(self, par1_sums: defaultdict, par2_sums: defaultdict, par1_label: str, par2_label: str):
+    def __init__(self, par1_label: str, par2_label: str, par1_sums: defaultdict, par2_sums: defaultdict):
+        self.par1_label = par1_label 
+        self.par2_label = par2_label
         self.par1_sums = par1_sums
         self.par2_sums = par2_sums
-        self.par1_label = par1_label
-        self.par2_label = par2_label
 
 
 df = pd.read_csv('results/addm_posteriors.csv')
@@ -21,50 +23,45 @@ df = pd.read_csv('results/addm_posteriors.csv')
 N = df.shape[1] - 1
 
 # Calculate the sums of probabilities for each parameter
-d_sums = defaultdict(int)
-sigma_sums = defaultdict(int)
-theta_sums = defaultdict(int) 
-for i, row in df.iterrows(): 
-    d_val = row['d']
-    sigma_val = row['sigma']
-    theta_val = row['theta']
-    p_val = row['p']
-    d_sums[d_val] += p_val
-    sigma_sums[sigma_val] += p_val
-    theta_sums[theta_val] += p_val
+param_sums: List[Tuple[str, defaultdict]] = [] 
+# Example entry: ('d', {0.005: 0.25, 0.006: 0.5, 0.005: 0.025})
+for param in df:
+    if param != PROB_LABEL:
+        param_dict = defaultdict(int)
+        for i, row in df.iterrows(): 
+            param_row_val = row[param] # Current value of the parameter
+            param_dict[param_row_val] += row[PROB_LABEL] # Probability determined given 
+                # that parameter and some combination of the other parameters
+        param_sums.append((param, param_dict))
 
-# Queue to store data for bar plots (diagonal subplots)
-diags: queue.Queue[defaultdict] = queue.Queue()
-diags.put(("d", d_sums))
-diags.put(("sigma", sigma_sums))
-diags.put(("theta", theta_sums))
-
-# Initialize arrays to store data for heatmaps
-d_sigma = np.zeros((len(d_sums), len(sigma_sums)))
-d_theta = np.zeros((len(d_sums), len(theta_sums)))
-sigma_theta = np.zeros((len(sigma_sums), len(theta_sums)))
-
-# Define a list of MapData objects to store information about each heatmap
-heatmaps = [
-    MapData(d_sums, sigma_sums, "d", "sigma"), 
-    MapData(d_sums, theta_sums, "d", "theta"), 
-    MapData(sigma_sums, theta_sums, "sigma", "theta")
-]
+# Create pairs of parameters for heatmaps
+# Iterate down each column starting with the first parameter
+heatmaps: List[MapData] = list()
+for i in range(N):
+    for j in range(i + 1, N):
+        heatmaps.append(MapData(
+            param_sums[i][0], param_sums[j][0], 
+                param_sums[i][1], param_sums[j][1]))
 
 # Queue to store data for heatmaps
 heatmaps_queue: queue.Queue[Tuple[MapData, np.ndarray]] = queue.Queue()
 
-# Iterate over each MapData object and calculate the sums for corresponding heatmap
+# Iterate over each MapData object and calculate the sums for the 
+# corresponding heatmap
 for map_data in heatmaps: 
     print(map_data.par1_label, map_data.par2_label, "p_sum")
-    arr_data = np.zeros((len(map_data.par1_sums), len(map_data.par2_sums)))
+    # Create empty heatmap data based on number of possible values for 
+    # each parameter
+    arr_data = np.zeros((len(map_data.par1_sums), len(map_data.par2_sums))) 
     for i in range(len(map_data.par1_sums)):
         for j in range(len(map_data.par2_sums)):
             curr_par1 = list(map_data.par1_sums.keys())[i] # first parameter
             curr_par2 = list(map_data.par2_sums.keys())[j] # second parameter
+            # Get the sum of all probabilities for the current pair of 
+            # parameter values
             for k, row in df.iterrows():
                 if row[map_data.par1_label] == curr_par1 and row[map_data.par2_label] == curr_par2:
-                    arr_data[i][j] += row["p"]
+                    arr_data[i][j] += row[PROB_LABEL]
             print(curr_par1, curr_par2, arr_data[i][j])
     heatmaps_queue.put((map_data, arr_data))
     print()
@@ -73,39 +70,52 @@ for map_data in heatmaps:
 fig, axes = plt.subplots(figsize=(10, 10), ncols=N, nrows=N)
 
 # Iterate over each subplot and plot either a bar plot or a heatmap
+diag_idx = 0 
 for i in range(N):
     for j in range(N):
         if i < j:
-            axes[i, j].axis('off')  # Turn off axes for subplots above the diagonal 
+            # Turn off axes for subplots above the diagonal 
+            axes[i, j].axis('off')  
         elif i == j:
-            curr = diags.get()
-            title = curr[0]
+            curr = param_sums[diag_idx]
             vals = curr[1]
-            g = sns.barplot(x=list(vals.keys()), y=list(vals.values()), ax=axes[i, j], color="grey")
-            g.bar_label(g.containers[0], size=8)  # Add labels to the bar plot bars
+            g = sns.barplot(
+                x=list(vals.keys()), 
+                y=list(vals.values()), 
+                ax=axes[i, j], 
+                color="grey"
+                )
+            g.bar_label(g.containers[0], size=8)  
 
             keys_labels = list(vals.keys())
             axes[i, j].set_xticks(np.arange(len(keys_labels)))
             axes[i, j].set_xticklabels(keys_labels)
+
+            diag_idx += 1
         else:
             curr_heatmap = heatmaps_queue.get()
             data = curr_heatmap[0]
             arr = curr_heatmap[1]
-            g = sns.heatmap(arr.T, annot=True, cbar=False, cmap="crest", ax=axes[i, j])
-            g.invert_yaxis()  # Invert y-axis for heatmap to match the order of values
+            g = sns.heatmap(
+                arr.T, 
+                annot=True, 
+                cbar=False, 
+                cmap="crest", 
+                ax=axes[i, j]
+                )
+            # Invert y-axis for heatmap to match the order of values
+            g.invert_yaxis()  
+            
 
             g.set_xticks(np.arange(len(data.par1_sums)) + 0.5, minor=False)  
-            g.set_xticklabels(list(data.par1_sums.keys()), minor=False)  # Center the x-axis ticks for the heatmap
+            g.set_xticklabels(list(data.par1_sums.keys()), minor=False)  
             g.set_yticks(np.arange(len(data.par2_sums)) + 0.5, minor=False)  
-            g.set_yticklabels(list(data.par2_sums.keys()), minor=False)  # Center the y-axis ticks for the heatmap
+            g.set_yticklabels(list(data.par2_sums.keys()), minor=False) 
 
 # Set labels for x and y axes of plot
-axes[0, 0].set_ylabel("d", size=16)
-axes[1, 0].set_ylabel("sigma", size=16)
-axes[2, 0].set_ylabel("theta", size=16)
-axes[2, 0].set_xlabel("d", size=16)
-axes[2, 1].set_xlabel("sigma", size=16)
-axes[2, 2].set_xlabel("theta", size=16)
+for i in range(N):
+    axes[i, 0].set_ylabel(df.columns[i], size=16)
+    axes[N - 1, i].set_xlabel(df.columns[i], size=16)
 
 plt.tight_layout()
 plt.show()
